d3da1b2a9c98e2f3b5e5afd09894564c73701ce6
roxanne <roxanne_chang@trend.com.tw>
2014-04-03 17:52:22 +0800

    init project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularFlappyBirdApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
