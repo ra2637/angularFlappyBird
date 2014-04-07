2dcb323bbc9164538fbf29c54c17e126d664df59
Kyle Yu <chaukwai@gmail.com>
2014-04-07 15:18:40 +0800

    continue dropping

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,10 +2,13 @@
 
 angular.module('flappyBirdApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
-        $scope.birdPosition = {top: 300, left: 550};
+        $scope.birdPosition = {top: 150, left: 550};
 
         $scope.setPosition = function(){
-            $scope.birdPosition = {top: $scope.birdPosition.top +20, left:550};
+            $scope.birdPosition = {top: $scope.birdPosition.top + 3, left:550};
+            if($scope.birdPosition.top > 300)
+            {
+                $scope.birdPosition = {top: 150, left: 550};
+            }
         };
-
   }]);
