9009a9b7c49b9bf407e25507c6adf9e25849d62c
ra2637 <ra2637@gmail.com>
2014-04-14 14:14:55 +0800

    modify produce pipe method and create bottom pipes (style not change yet.)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,6 +4,7 @@ angular.module('flappyBirdApp')
   .controller('MainCtrl', ['$scope','$interval', function ($scope,$interval) {
     $scope.birdPosition = {top: 150, left: 550};
     var screenHeight = window.innerHeight;
+    var screenWidth = window.innerWidth;
     var containerPosition = angular.element(document.getElementsByClassName('gameContainer')).position();
     $scope.birdVelocity = 0;
 	$scope.birdAccerlator = 0.3;
@@ -27,8 +28,10 @@ angular.module('flappyBirdApp')
 
     $scope.initPipes = function(){
         $scope.pipes = [];
-        for(var i=0; i<5; i++){
+        $scope.pipesBottom = [];
+        for(var i=0; i<1; i++){
             $scope.pipes.push(createPipe(i));
+            $scope.pipesBottom.push(createPipe(i));
         }
     }
 
@@ -37,7 +40,7 @@ angular.module('flappyBirdApp')
             height: generatePipeHeight(100, 250),
             position: {
                 top: 0,
-                left: 1200 + 160*2*index
+                left: (screenWidth-700)/2+700+ (230+80)*index
             }
         }
     }
@@ -49,11 +52,18 @@ angular.module('flappyBirdApp')
     $scope.setPipePosition = function(){
         for(var i=0; i<$scope.pipes.length; i++){
             var pipe = $scope.pipes[i];
+            // pipe.position.left = pipe.position.left - 5;
             pipe.position.left = pipe.position.left - 5;
+
+            if(i==$scope.pipes.length-1)
+            {
+                if((containerPosition.left+700-pipe.position.left) > 310){
+                    $scope.pipes.push(createPipe(0));
+                }
+            }
 
             if(pipe.position.left+80 < containerPosition.left){
                 $scope.pipes.shift();
-                $scope.pipes.push(createPipe(3));
             }
         }
     }
